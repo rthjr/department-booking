11f8a7f0a2f45dde859d0c5ba7325113016b13ff
Potato <137735702+rthjr@users.noreply.github.com>
2024-09-19 23:13:16 +0700

    d

--- js/navbar.js
+++ js/navbar.js
@@ -1,19 +1,4 @@
-window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('.navbar');
-    const navLinks = document.querySelectorAll('.nav-navbar .nav-link');
-    const scrollThreshold = window.innerHeight * 0.3; 
-    if (window.scrollY > scrollThreshold) {
-        navbar.classList.add('scrolled'); 
-        navLinks.forEach((link) => {
-            link.classList.add('scrolled');
-        });
-    } else {
-        navbar.classList.remove('scrolled');  
-        navLinks.forEach((link) => {
-            link.classList.remove('scrolled');
-        });
-    }
-});
+
 window.addEventListener('scroll', function () {
     const navbar = document.querySelector('.navbar');
     const navLinks = document.querySelectorAll('.navbar .nav-login button a');
